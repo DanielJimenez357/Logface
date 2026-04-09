be28caef7a66323079faff6619895cb01d23b197
DanielJimenez357 <daniel05jimenez357@gmail.com>
2026-04-09 20:15:57 +0200

    fin del apartado calendario

--- frontend/src/components/calendar/Calendar.jsx
+++ frontend/src/components/calendar/Calendar.jsx
@@ -1,8 +1,58 @@
+import Day from './Day.jsx'
+
 function Calendar() {
+  const monthData = [
+  { schedule: "08:00 - 16:00", isHoliday: false },
+  { schedule: "08:00 - 16:00", isHoliday: false },
+  { schedule: "08:00 - 16:00", isHoliday: false },
+  { schedule: "08:00 - 16:00", isHoliday: false },
+  { schedule: "08:00 - 16:00", isHoliday: false },
+  { schedule: "Closed", isHoliday: true },
+  { schedule: "Closed", isHoliday: true },
+  { schedule: "09:00 - 17:00", isHoliday: false },
+  { schedule: "09:00 - 17:00", isHoliday: false },
+  { schedule: "09:00 - 17:00", isHoliday: false },
+  { schedule: "09:00 - 17:00", isHoliday: false },
+  { schedule: "09:00 - 17:00", isHoliday: false },
+  { schedule: "Closed", isHoliday: true },
+  { schedule: "Closed", isHoliday: true },
+  { schedule: "Closed", isHoliday: true },
+  { schedule: "08:30 - 16:30", isHoliday: false },
+  { schedule: "08:30 - 16:30", isHoliday: false },
+  { schedule: "08:30 - 16:30", isHoliday: false },
+  { schedule: "08:30 - 16:30", isHoliday: false },
+  { schedule: "Closed", isHoliday: true },
+  { schedule: "Closed", isHoliday: true },
+  { schedule: "09:00 - 18:00", isHoliday: false },
+  { schedule: "09:00 - 18:00", isHoliday: false },
+  { schedule: "09:00 - 18:00", isHoliday: false },
+  { schedule: "Closed", isHoliday: true },
+  { schedule: "09:00 - 18:00", isHoliday: false },
+  { schedule: "09:00 - 18:00", isHoliday: false },
+  { schedule: "Closed", isHoliday: true },
+  { schedule: "Closed", isHoliday: true },
+  { schedule: "08:00 - 14:00", isHoliday: false }
+]; 
   return (
   <>
-    <div>
-      <p>Calendario de este mes</p>
+    <div className=" w-full h-9/10">
+      <p className="w-full text-center">Calendario de este mes</p>
+        <div className="bg-white rounded-t-lg border border-negro1 h-full w-full">
+          <div className="grid grid-cols-7 text-center border-b h-[5%] border-negro1">
+            <p>Lunes</p>
+            <p>Martes</p>
+            <p>Miercoles</p>
+            <p>Jueves</p>
+            <p>Viernes</p>
+            <p>Sabado</p>
+            <p>Domingo</p>
+          </div>
+          <div className="grid grid-cols-7 h-[95%] ">
+            {monthData.map((item, index) => (
+            <Day schedule={item.schedule} isHoliday={item.isHoliday}/>
+            ))} 
+          </div>
+        </div>
     </div>
     </>
   )
--- frontend/src/components/calendar/Day.jsx
+++ frontend/src/components/calendar/Day.jsx
@@ -1,1 +1,14 @@
+function Day({ schedule, isHoliday}) {
 
+  const style = `w-full border border-negro1 text-center auto-rows-fr flex items-center justify-center vertical hover:brightness-90 transition-colors ${isHoliday ? "bg-rojo1 text-white" : "bg-white"}`
+
+  return (
+  <>
+      <div className={style}>
+        <p>{schedule}</p>
+      </div>
+    </>
+  )
+}
+
+export default Day
