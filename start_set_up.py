69559cec1f8270701215504a2327e5a0937f2fb4
DanielJimenez357 <daniel05jimenez357@gmail.com>
2026-05-01 13:10:06 +0200

    precambios estilos en frontend

---
+++ start_set_up.py
@@ -0,0 +1,12 @@
+import subprocess
+
+scripts = [
+    ". ./venv/bin/activate; cd backend/; python3 manage.py runserver; exec bash",
+    "cd frontend; npm run dev; exec bash",
+    "kitty --session /home/daniel/Desktop/proyects/tfg/session.kitty-session; exec bash",
+    "cd docker/; sudo docker-compose up; exec bash"
+]
+
+for script in scripts:
+    full_script = ['gnome-terminal', '--', 'bash', '-c', script]
+    subprocess.Popen(full_script)
