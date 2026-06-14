76ea13b29199c4f09a67eeb266caaa63b38add7c
DanielJimenez357 <daniel05jimenez357@gmail.com>
2026-06-14 22:06:10 +0200

    prueba despliegue 15

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -23,8 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
-
+# See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-hc2uap9#(uj&*)3w%x_07rhm@z!h1=flq95^o2zbr7gfv-pnf8"
 
