d6144de4d3b57fee71857e1002d4c58ace080a48
DanielJimenez357 <daniel05jimenez357@gmail.com>
2026-06-14 21:24:10 +0200

    prueba despliegue 11

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-#
+#1
 import os
 import ldap
 import dj_database_url
